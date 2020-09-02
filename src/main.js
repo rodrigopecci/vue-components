c056e46dcaffd24bdc8ce8cb92d04f21f78eee4c
Rodrigo Pecci Acorse <rodrigopeccirpa@gmail.com>
2020-09-02 11:45:46 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import components here
+import SampleComponent from "./components/SampleComponent.vue";
+import AnotherSampleComponent from "./components/AnotherSampleComponent.vue";
+
+// export components for this library
+export default {
+  SampleComponent,
+  AnotherSampleComponent
+};
